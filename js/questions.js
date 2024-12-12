07f93c34996bf9990f659d77682c8cd301a85524
Hafsa-Nawas <nfathimahafsa0101@gmail.com>
2024-12-13 00:17:51 +0530

    Files Addes

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following attribute is used to add link to any element?",
+    answer: "href",
+    options: [
+      "link",
+      "ref",
+      "href",
+      "newref"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How do you declare a JavaScript variable?",
+    answer: "var carName;",
+    options: [
+      "var carName;",
+      "variable carName;",
+      "v carName;",
+      "none of these"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following attribute is used to provide a unique name to an element?",
+    answer: "id",
+    options: [
+      "type",
+      "data",
+      "id",
+      "none of the above"
+    ]
+  },
+];
